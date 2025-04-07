f92374519ad2b1a1af4fc036401ae4ba480d4f9d
liangcechinese <849206983@qq.com>
2025-04-07 23:11:14 +0800

    Fix type import in CompressionOptions component

--- src/components/CompressionOptions.tsx
+++ src/components/CompressionOptions.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { CompressionOptions } from '@/utils/imageCompression';
+import type { CompressionOptions } from '@/utils/imageCompression';
 
 interface CompressionOptionsProps {
   options: CompressionOptions;
